89d7a82aceaac360402446118c369fa5366d04a3
Colin Williams <lackita@gmail.com>
2015-03-07 21:28:25 -0500

    removed configuration

--- src/name/colinwilliams/zombiesimulator/Main.java
+++ src/name/colinwilliams/zombiesimulator/Main.java
@@ -2,7 +2,6 @@ package name.colinwilliams.zombiesimulator;
 
 public class Main {
     public static void main(String[] args) {
-	//Configuration cfg = new Configuration(args[0]);
 	Battlefield b = new Battlefield(1000);
 	b.begin();
     }
